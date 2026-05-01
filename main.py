51a4413011a56541316293f42be7466e44b8c8e2
nikhilshetye26 <nikhilshetye26@gmail.com>
2026-05-01 12:00:33 +0530

    Add charts

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from src.analysis import analyze_stock
 from src.signals import generate_signals
+import matplotlib.pyplot as plt
+
 
 def main():
     df = pd.read_csv("data/stock.csv")
@@ -11,6 +13,8 @@ def main():
     df = generate_signals(df)
 
     print(df.tail())
+    df["CLOSE"].plot()
+    plt.show()
 
 if __name__ == "__main__":
     main()
